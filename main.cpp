c734688c1cfb5232f8bb19efbd3765b3049e3d92
mushcatshiro <yingjieho@hotmail.com>
2025-08-22 22:27:45 +0800

    init au

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ constexpr float ViewportWidth = 800.f;
 constexpr float ViewportHeight = 800.f;
 constexpr float t = 86400.f;  // 1 tick = 1 hour (3600 seconds)
 constexpr float m2vp = (ViewportWidth) / (440000000.f * 2); // pix/m
-constexpr float au2vp = (ViewportWidth) / (55.f * 2) ;  // pix/au
+constexpr float au2vp = (ViewportWidth) / (MoondDistanceFromEarth * 2.f);  // pix/au
 constexpr size_t ListLength = 32;
 constexpr int HeadGreyValue = 200;
 
@@ -51,12 +51,12 @@ struct CelestialObj {
   }
 
   void calculateAcceleration(std::vector<CelestialObj*> celestialBodies) {
-    // acceleration = glm::vec2(0.f, 0.f);
     this->acceleration.x = 0.0f;
     this->acceleration.y = 0.0f;
     for (CelestialObj* c : celestialBodies) {
       if (c->name != this->name) {
         glm::vec2 direction = glm::vec2{c->x, c->y} - glm::vec2{this->x, this->y};
+        direction *= AU;
         float r = glm::length(direction);  // sqrt((x^2) + (y^2))
         glm::vec2 unitDirection = direction / r;
         this->acceleration += c->mass / (r * r) * unitDirection;
@@ -67,9 +67,8 @@ struct CelestialObj {
 
   void updatePos(glm::vec2 acceleration, bool debug) {
     sf::CircleShape trailPoint(radius * m2vp); // Adjust the size and color as needed
-    // trailPoint.setFillColor(sf::Color(color.r, color.r, color.r, 127)); // Slightly transparent
     trailPoint.setOrigin({trailPoint.getRadius(), trailPoint.getRadius()});
-    trailPoint.setPosition({ViewportWidth / 2.f + x * m2vp, ViewportHeight / 2.f + y * m2vp});
+    trailPoint.setPosition({ViewportWidth / 2.f + x * au2vp, ViewportHeight / 2.f + y * au2vp});
     trail.push_back(trailPoint);
     if (trail.size() >= ListLength) {
       trail.erase(trail.begin());
@@ -77,8 +76,8 @@ struct CelestialObj {
 
     this->vx += acceleration.x * t;
     this->vy += acceleration.y * t;
-    this->x += vx * t;
-    this->y += vy * t;
+    this->x += (vx * t) / AU;
+    this->y += (vy * t) / AU;
     if (debug) {
       std::cout << "vx, vy:" << vx << "," << vy << std::endl;
       std::cout << "x, y:" << x << "," << y << std::endl;
@@ -106,7 +105,7 @@ struct CelestialObj {
     
     shape.setOrigin({shape.getRadius(), shape.getRadius()});
     shape.setFillColor(color);
-    shape.setPosition({ViewportWidth / 2.f + x * m2vp, ViewportHeight / 2.f + y * m2vp});
+    shape.setPosition({ViewportWidth / 2.f + x * au2vp, ViewportHeight / 2.f + y * au2vp});
     window.draw(shape);
   };
 };
@@ -115,7 +114,7 @@ CelestialObj sun{"sun", 0., 0., SunRadius, SunMass, sf::Color{200, 200, 10}, fal
 CelestialObj mecury{"mecury", MecuryDistanceFromSun, 0., MecuryRadius, MecuryMass, sf::Color(116, 123, 129), true};
 CelestialObj venus{"venus", VenusDistanceFromSun, 0.,  VenusRadius, VenusMass, sf::Color(255, 198, 73), true};
 CelestialObj earth{"earth", 0., 0., EarthRadius, EarthMass, sf::Color(159, 193, 100), false};
-CelestialObj moon{"moon", 3.633e8, 0., MoonRadius, MoonMass, sf::Color(201, 201, 201), true};
+CelestialObj moon{"moon", MoondDistanceFromEarth, 0., MoonRadius, MoonMass, sf::Color(201, 201, 201), true};
 CelestialObj mars{"mars", MarsDistanceFromSun, 0., MarsRadius, MarsMass, sf::Color(129, 37, 27), true};
 CelestialObj jupiter{"jupiter", JupiterDistanceFromSun, 0., JupiterRadius, JupiterMass, sf::Color(188, 175, 178), true};
 CelestialObj saturn{"saturn", SaturnDistanceFromSun, 0., SaturnRadius, SaturnMass, sf::Color(196, 188, 170), true};
@@ -124,15 +123,6 @@ CelestialObj neptune{"neptune", NeptuneDistanceFromSun, 0., NeptuneRadius, Neptu
 CelestialObj pluto{"pluto", PlutoDistanceFromSun, 0., PlutoRadius, PlutoMass, sf::Color(146, 168, 164), true};
 
 std::vector<CelestialObj*> solarSystem = {&earth, &moon};
-
-glm::vec2 calculateGForce(const CelestialObj& p1, const CelestialObj& p2) {
-  // calculates f `p1` exerts on `p2`, direction is towards `p1`
-  glm::vec2 direction = glm::vec2{p1.x, p1.y} - glm::vec2{p2.x, p2.y};
-  float r = glm::length(direction);  // sqrt((x^2) + (y^2))
-  glm::vec2 unitDirection = direction / r;
-  float mag = GravitationalConstant * p1.mass * p2.mass / (r * r);
-  return mag * unitDirection;
-};
 
 int main() {
   /*
@@ -145,7 +135,7 @@ int main() {
   int ctr = 0;
 
   // Calculate initial orbital velocity for a stable circular orbit
-  moon.vy = 1.076e3;
+  moon.vy = -1.076e3;
   earth.vy = -(moon.vy * moon.mass) / earth.mass; // conservation of momentum
 
   // run the program as long as the window is open
@@ -157,21 +147,12 @@ int main() {
         window.close();
     }
 
-    // glm::vec2 F = calculateGForce(earth, moon);
-    // glm::vec2 accelEarth = -F / earth.mass;
-    // glm::vec2 accelMoon = F / moon.mass;
-    // earth.updatePos(accelEarth, false);
-    // moon.updatePos(accelMoon, false);
     for (CelestialObj* c: solarSystem) {
       c->calculateAcceleration(solarSystem);
     }
     for (CelestialObj* c: solarSystem) {
-      c->updatePos(c->acceleration, false);
+      c->updatePos(c->acceleration, true);
     }
-    // earth.calculateAcceleration(solarSystem);
-    // moon.calculateAcceleration(solarSystem);
-    // earth.updatePos(earth.acceleration, false);
-    // moon.updatePos(moon.acceleration, false);
 
     // clear the window with black color
     window.clear(sf::Color::Black);
