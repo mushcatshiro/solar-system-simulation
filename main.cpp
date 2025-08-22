0f91ff88bf5ffcb930870d4ba69d7a03b0d4f85b
mushcatshiro <yingjieho@hotmail.com>
2025-08-22 17:32:37 +0800

    interesting bug

--- main.cpp
+++ main.cpp
@@ -23,9 +23,10 @@ cpp float range [3.4e-38, 3.4e+38]
 constexpr float ViewportWidth = 800.f;
 constexpr float ViewportHeight = 800.f;
 constexpr float t = 86400.f;  // 1 tick = 1 hour (3600 seconds)
-constexpr float m2vp = (ViewportWidth) / (440000000.f * 2); // pix/m
+constexpr float m2vp = (ViewportWidth) / (1000000000.f * 2); // pix/m
+constexpr float m2vp2 = (ViewportWidth) / (440000000.f * 2);
 constexpr float au2vp = (ViewportWidth) / (55.f * 2) ;  // pix/au
-constexpr size_t ListLength = 32;
+constexpr size_t ListLength = 64;
 constexpr int HeadGreyValue = 200;
 
 struct CelestialObj {
@@ -43,23 +44,23 @@ struct CelestialObj {
   glm::vec2 acceleration;
 
   CelestialObj(std::string n, float x_m, float y_m, float r_m, float m_kg, sf::Color c, bool st)
-    : name(n), x(x_m), y(y_m), radius(r_m), mass(m_kg), color(c), shape(r_m * m2vp), showTrail(st) {
+    : name(n), x(x_m), y(y_m), radius(r_m), mass(m_kg), color(c), shape(r_m * m2vp2), showTrail(st) {
     vx = 0.f;
     vy = 0.f;
     acceleration.x = 0.0f;
     acceleration.y = 0.0f;
   }
 
-  void calculateAcceleration(std::vector<CelestialObj*> celestialBodies) {
+  void calculateAcceleration(std::vector<CelestialObj> celestialBodies) {
     // acceleration = glm::vec2(0.f, 0.f);
     this->acceleration.x = 0.0f;
     this->acceleration.y = 0.0f;
-    for (CelestialObj* c : celestialBodies) {
-      if (c->name != this->name) {
-        glm::vec2 direction = glm::vec2{c->x, c->y} - glm::vec2{this->x, this->y};
+    for (CelestialObj c : celestialBodies) {
+      if (c.name != this->name) {
+        glm::vec2 direction = glm::vec2{c.x, c.y} - glm::vec2{this->x, this->y};
         float r = glm::length(direction);  // sqrt((x^2) + (y^2))
         glm::vec2 unitDirection = direction / r;
-        this->acceleration += c->mass / (r * r) * unitDirection;
+        this->acceleration += c.mass / (r * r) * unitDirection;
       }
     }
     this->acceleration *= GravitationalConstant;
@@ -114,7 +115,7 @@ struct CelestialObj {
 CelestialObj sun{"sun", 0., 0., SunRadius, SunMass, sf::Color{200, 200, 10}, false};
 CelestialObj mecury{"mecury", MecuryDistanceFromSun, 0., MecuryRadius, MecuryMass, sf::Color(116, 123, 129), true};
 CelestialObj venus{"venus", VenusDistanceFromSun, 0.,  VenusRadius, VenusMass, sf::Color(255, 198, 73), true};
-CelestialObj earth{"earth", 0., 0., EarthRadius, EarthMass, sf::Color(159, 193, 100), false};
+CelestialObj earth{"earth", 0., 0., EarthRadius, EarthMass, sf::Color(159, 193, 100), true};
 CelestialObj moon{"moon", 3.633e8, 0., MoonRadius, MoonMass, sf::Color(201, 201, 201), true};
 CelestialObj mars{"mars", MarsDistanceFromSun, 0., MarsRadius, MarsMass, sf::Color(129, 37, 27), true};
 CelestialObj jupiter{"jupiter", JupiterDistanceFromSun, 0., JupiterRadius, JupiterMass, sf::Color(188, 175, 178), true};
@@ -123,7 +124,8 @@ CelestialObj uranus{"uranus", UranusDistanceFromSun, 0., UranusRadius, UranusMas
 CelestialObj neptune{"neptune", NeptuneDistanceFromSun, 0., NeptuneRadius, NeptuneMass, sf::Color(124, 183, 187), true};
 CelestialObj pluto{"pluto", PlutoDistanceFromSun, 0., PlutoRadius, PlutoMass, sf::Color(146, 168, 164), true};
 
-std::vector<CelestialObj*> solarSystem = {&earth, &moon};
+// std::vector<CelestialObj*> solarSystem = {&earth, &moon};
+std::vector<CelestialObj> solarSystem = {earth, moon};
 
 glm::vec2 calculateGForce(const CelestialObj& p1, const CelestialObj& p2) {
   // calculates f `p1` exerts on `p2`, direction is towards `p1`
@@ -162,16 +164,16 @@ int main() {
     // glm::vec2 accelMoon = F / moon.mass;
     // earth.updatePos(accelEarth, false);
     // moon.updatePos(accelMoon, false);
-    for (CelestialObj* c: solarSystem) {
-      c->calculateAcceleration(solarSystem);
-    }
-    for (CelestialObj* c: solarSystem) {
-      c->updatePos(c->acceleration, false);
-    }
-    // earth.calculateAcceleration(solarSystem);
-    // moon.calculateAcceleration(solarSystem);
-    // earth.updatePos(earth.acceleration, false);
-    // moon.updatePos(moon.acceleration, false);
+    // for (CelestialObj* c: solarSystem) {
+    //   c->calculateAcceleration(solarSystem);
+    // }
+    // for (CelestialObj* c: solarSystem) {
+    //   c->updatePos(c->acceleration, false);
+    // }
+    earth.calculateAcceleration(solarSystem);
+    moon.calculateAcceleration(solarSystem);
+    earth.updatePos(earth.acceleration, false);
+    moon.updatePos(moon.acceleration, false);
 
     // clear the window with black color
     window.clear(sf::Color::Black);
@@ -183,7 +185,7 @@ int main() {
     ctr += 1;
     // std::cout << "ctr: " << ctr << std::endl;
     // std::this_thread::sleep_for(std::chrono::microseconds(100));
-    // std::this_thread::sleep_for(std::chrono::seconds(1)); 
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   if (std::fetestexcept(FE_OVERFLOW)) {
     std::cout << "Floating-point overflow detected!" << std::endl;
